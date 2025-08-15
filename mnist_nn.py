5b4d59ea7d1edf6f65370654b47e85baab71293c
Harutyun Khachatryan <kh_harut@yahoo.com>
2025-08-15 13:25:42 +0200

    removing additional layer

--- mnist_nn.py
+++ mnist_nn.py
@@ -13,8 +13,6 @@ y_test = to_categorical(y_test)
 model = Sequential()
 model.add(Input(shape=(28, 28, 1)))
 model.add(Conv2D(64, kernel_size=3, activation='relu'))
-model.add(Conv2D(64, kernel_size=3, activation='relu',
-                 input_shape=(28, 28, 1)))
 model.add(Conv2D(32, kernel_size=3, activation='relu'))
 model.add(Flatten())
 model.add(Dense(10, activation='softmax'))
